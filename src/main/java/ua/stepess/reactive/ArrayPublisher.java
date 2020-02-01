8c0aa97964177721cd772856876c02b518332b58
Stepess <stepanersh@gmail.com>
2020-02-01 17:08:41 +0200

    finish publisher

--- src/main/java/ua/stepess/reactive/ArrayPublisher.java
+++ src/main/java/ua/stepess/reactive/ArrayPublisher.java
@@ -15,19 +15,64 @@ public class ArrayPublisher<T> implements Publisher<T> {
     @Override
     public void subscribe(Subscriber<? super T> subscriber) {
         subscriber.onSubscribe(new Subscription() {
+
+            int index;
+
+            long requested;
+
+            boolean cancelled;
+
             @Override
             public void request(long n) {
 
+                if (n <= 0 && !cancelled){
+                    cancel();
+                    subscriber.onError(new IllegalArgumentException());
+                    return;
+                }
+
+                long initialRequested = requested;
+
+                requested += n;
+
+                if (initialRequested != 0) {
+                    return;
+                }
+
+                int sent = 0;
+
+                for (; sent < requested && index < array.length; sent++, index++) {
+                    if (cancelled) {
+                        return;
+                    }
+
+                    T element = array[index];
+
+                    if (element == null) {
+                        subscriber.onError(new NullPointerException());
+                        return;
+                    }
+
+                    subscriber.onNext(element);
+                }
+
+                if (cancelled) {
+                    return;
+                }
+
+                if (index == array.length) {
+                    subscriber.onComplete();
+                    return;
+                }
+
+                requested -= sent;
             }
 
             @Override
             public void cancel() {
-
+                cancelled = true;
             }
         });
-        for (int i = 0; i < array.length; i++) {
-            subscriber.onNext(array[i]);
-        }
-        subscriber.onComplete();
+
     }
 }
--- src/test/java/ua/stepess/reactive/ArrayPublisherTest.java
+++ src/test/java/ua/stepess/reactive/ArrayPublisherTest.java
@@ -2,17 +2,35 @@ package ua.stepess.reactive;
 
 
 import org.junit.Test;
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.reactivestreams.tck.PublisherVerification;
+import org.reactivestreams.tck.TestEnvironment;
 
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ArrayPublisherTest {
+public class ArrayPublisherTest extends PublisherVerification<Long> {
+
+    public ArrayPublisherTest() {
+        super(new TestEnvironment());
+    }
+
+    @Override
+    public Publisher<Long> createPublisher(long elements) {
+        return new ArrayPublisher<>(generate(elements));
+    }
+
+    @Override
+    public Publisher<Long> createFailedPublisher() {
+        return null;
+    }
 
     @Test
     public void everyMethodInSubscriberShouldBeExecutedInParticularOrder() throws InterruptedException {
@@ -59,6 +77,165 @@ public class ArrayPublisherTest {
                         "onComplete()"
                 );
 
+    }
+
+    @Test
+    public void mustSupportBackpressureControl() throws InterruptedException {
+        var countDownLatch = new CountDownLatch(1);
+        var collected = new ArrayList<Long>();
+        var subscriptionHolder = new Subscription[1];
+        long toReq = 5L;
+        var array = generate(toReq);
+        var arrayPublisher = new ArrayPublisher<>(array);
+
+        arrayPublisher.subscribe(new Subscriber<Long>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                subscriptionHolder[0] = s;
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                collected.add(aLong);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                countDownLatch.countDown();
+            }
+        });
+
+        assertThat(collected).isEmpty();
+
+        var subscription = subscriptionHolder[0];
+
+        subscription.request(1);
+        assertThat(collected).containsExactly(0L);
+
+        subscription.request(1);
+        assertThat(collected).containsExactly(0L, 1L);
+
+        subscription.request(2);
+        assertThat(collected).containsExactly(0L, 1L, 2L, 3L);
+
+        subscription.request(20);
+
+        assertThat(countDownLatch.await(1, TimeUnit.SECONDS)).isTrue();
+        assertThat(collected).containsExactly(array);
+    }
+
+    @Test
+    public void testNullability() throws InterruptedException {
+        var countDownLatch = new CountDownLatch(1);
+        var array = new Long[]{null};
+        var error = new AtomicReference<Throwable>();
+        var arrayPublisher = new ArrayPublisher<>(array);
+
+        arrayPublisher.subscribe(new Subscriber<Long>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(1);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                error.set(t);
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        });
+
+        countDownLatch.await(1, TimeUnit.SECONDS);
+
+        assertThat(error.get()).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void testRecursion() throws InterruptedException {
+        var countDownLatch = new CountDownLatch(1);
+        var collected = new ArrayList<Long>();
+        long toReq = 1000L;
+        var array = generate(toReq);
+        var arrayPublisher = new ArrayPublisher<>(array);
+
+        arrayPublisher.subscribe(new Subscriber<Long>() {
+            Subscription s;
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                this.s = s;
+                s.request(1);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                collected.add(aLong);
+                s.request(1);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                countDownLatch.countDown();
+            }
+        });
+
+
+        assertThat(countDownLatch.await(5, TimeUnit.SECONDS)).isTrue();
+        assertThat(collected).containsExactly(array);
+    }
+
+    @Test
+    public void cancellationShouldWork() throws InterruptedException {
+        var countDownLatch = new CountDownLatch(1);
+        var collected = new ArrayList<Long>();
+        long toReq = 1000L;
+        var array = generate(toReq);
+        var arrayPublisher = new ArrayPublisher<>(array);
+
+        arrayPublisher.subscribe(new Subscriber<Long>() {
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.cancel();
+                s.request(toReq);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                collected.add(aLong);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                countDownLatch.countDown();
+            }
+        });
+
+
+        assertThat(countDownLatch.await(1, TimeUnit.SECONDS)).isFalse();
+        assertThat(collected).isEmpty();
     }
 
     static Long[] generate(long num) {
